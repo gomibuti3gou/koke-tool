c70fe34db64b91f8cb38d4775c3b30e1bc838432
gomibuti3gou <106477048+gomibuti3gou@users.noreply.github.com>
2022-10-17 01:49:11 +0900

    全てのパラメータを表示

--- exif_csv/main.go
+++ exif_csv/main.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -111,6 +113,80 @@ func setLatLng(name string, flag *bool) [][]string {
 	return photo
 }
 
+func P(t interface{}) string {
+	fmt.Println(reflect.TypeOf((t)))
+	return fmt.Sprintf("%s", reflect.TypeOf(t))
+}
+
+func change_type(value interface{}) string {
+	var res string
+	v := P(value)
+	fmt.Println(v)
+	switch v {
+	case "int":
+		res = fmt.Sprintf("%d", value)
+	case "float32":
+		res = fmt.Sprintf("%f", value)
+	case "float64":
+		res = fmt.Sprintf("%f", value)
+	case "string":
+		res = fmt.Sprintf("%s", value)
+	case "[]string":
+		vv, _ := value.([]string)
+		res = strings.Join(vv, " ")
+	case "[]int":
+		vv, _ := value.([]string)
+		res = strings.Join(vv, " ")
+	case "[]float64":
+		vv, _ := value.([]string)
+		res = strings.Join(vv, " ")
+	case "[]interface{}":
+		vv, _ := value.([]string)
+		res = strings.Join(vv, " ")
+	default:
+		res = fmt.Sprint(value)
+	}
+	return res
+}
+func allData(name string, label bool) ([]string, error) {
+	var photo []string
+	var key []string
+	file, err := os.Open(name)
+	if err != nil {
+		return photo, err
+	}
+	exif, err := exif.Decode(file)
+	if err != nil {
+		return photo, err
+	}
+	jdata, err := exif.MarshalJSON()
+	if err != nil {
+		return photo, err
+	}
+	var jsonObj interface{}
+	_ = json.Unmarshal(jdata, &jsonObj)
+	var datas = jsonObj.(map[string]interface{})
+	fmt.Println(datas)
+	for k, _ := range datas {
+		key = append(key, k)
+	}
+	if label {
+		sort.Strings(key)
+		key = append([]string{"ImageName"}, key...)
+		return key, err
+	}
+	sort.Strings(key)
+	for i := 0; i < len(key); i++ {
+		var str string
+		str = change_type(datas[key[i]])
+		photo = append(photo, str)
+	}
+	photo = append([]string{getFileName(name)}, photo...)
+
+	fmt.Println(photo)
+	return photo, err
+}
+
 func setSelectData(name string, info ...string) []string {
 	file, err := os.Open(name)
 	if err != nil {
@@ -144,9 +220,14 @@ func setSelectData(name string, info ...string) []string {
 }
 
 func main() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Printf("Current Folder %s \n", pwd)
+	//allData("/Users/oomorinichinichiki/Programs/Go/koke-tool/exif_csv/IMG_3567.jpeg", true)
 
 	var path string
-	pwd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
@@ -154,6 +235,7 @@ func main() {
 	fmt.Println("Input Folder Path ↓↓")
 	fmt.Scan(&path)
 	data := dirwalk(path)
+	//fmt.Println(data)
 
 	var name string
 	fmt.Println("File Name??")
@@ -164,11 +246,28 @@ func main() {
 	}
 
 	var photos [][]string
-
 	var seting string
-	fmt.Println("default or custom??")
+	var flag bool = true
+	fmt.Println("default or custom or all ??")
 	fmt.Scan(&seting)
-	if seting != "custom" {
+
+	if seting == "all" {
+		for _, d := range data {
+			if flag {
+				photo, err := allData(d, true)
+				if err != nil {
+					continue
+				}
+				photos = append(photos, photo)
+				flag = false
+			}
+			photo, err := allData(d, false)
+			if err != nil {
+				continue
+			}
+			photos = append(photos, photo)
+		}
+	} else if seting != "custom" {
 		photos = append(photos, setLabel("lat", "lng"))
 		for _, d := range data {
 			if !strings.Contains(d, "._") {
